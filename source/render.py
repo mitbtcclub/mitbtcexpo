83f17d8c06456ee4d919ed4ffdbfac1a40e41450
Firescar96 <nchinda2@gmail.com>
2017-02-14 01:09:59 -0500

    update to signal open registration

--- source/render.py
+++ source/render.py
@@ -4,15 +4,17 @@ import csv
 from tornado import template
 loader = template.Loader(".")
 
-# scsv= open("speakers.psv", "r").read()
+scsv= open("speakers.csv", "r").read()
 speakers=[]
-# speakerprefix="./assets/images/headshots/"
-# for row in csv.reader(scsv.split('\n'), delimiter='|'):
-#     row = list(row)
-#     row[0]=speakerprefix+row[0]
-#     speakers.append(row)
-#
-# scsv= open("day1.csv", "r").read()
+speakerprefix="./assets/images/headshots/"
+for row in csv.reader(scsv.split('\n'), delimiter='|'):
+    row = list(row)
+    if row == []:
+        break
+    row[0]=speakerprefix+row[0]
+    speakers.append(row)
+
+scsv= open("day1.csv", "r").read()
 day1= []
 # for row in csv.reader(scsv.split('\n'), delimiter='|'):
 #     time = row[0]+" - "+row[1]
@@ -25,11 +27,11 @@ day2= []
 #     day2.append([time]+row[2:])
 
 sponsorprefix = "./assets/images/sponsors/"
-bitcoinsponsors = [["fidelity.png", "fidelity"]]
+bitcoinsponsors = [["bitmain.png","bitmain"]]
 bitcoinsponsors = map(lambda x: [sponsorprefix+x[0]]+x[1:],bitcoinsponsors)
-litecoinsponsors = [["bitfury.png","bitfury"],["bitmain.png","bitmain"]]
+litecoinsponsors = []
 litecoinsponsors = map(lambda x: [sponsorprefix+x[0]]+x[1:],litecoinsponsors)
-mediasponsors = [["ben.png","ben"],["teradek.png","teradek3"]]
+mediasponsors = []
 mediasponsors = map(lambda x: [sponsorprefix+x[0]]+x[1:],mediasponsors)
 with open("index.html", "w") as f:
     f.write(loader.load("index.tmpl").generate(speakers=speakers,day1=day1,day2=day2,bitcoinsponsors=bitcoinsponsors,litecoinsponsors=litecoinsponsors,mediasponsors=mediasponsors))
