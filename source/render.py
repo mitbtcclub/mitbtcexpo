cab13205c2e8ce9b2d3ca15e47acb7fa71bfeba9
Firescar96 <nchinda2@gmail.com>
2018-03-12 23:13:55 -0400

    update speaker, and sponsors

--- source/render.py
+++ source/render.py
@@ -48,13 +48,11 @@ for row in csv.reader(scsv.split('\n'), delimiter='|'):
 sponsorprefix = "./assets/images/sponsors/"
 platinumsponsors = [["circle.png","Circle Wallet", "https://www.circle.com/en/"]]
 platinumsponsors = map(lambda x: [sponsorprefix+x[0]]+x[1:],platinumsponsors)
-goldsponsors = [["arcc.jpg","ARCC", "http://ibmr.io"], ["zkcapital.png","zkCapital", "https://zk.capital"], ["nucypher.svg","NuCypher", "https://www.nucypher.com"], ["zion.png","City of Zion", "https://cityofzion.io"], ["pillar.png", "Pillar VC", "https://pillar.vc"], ["inblockchain.png", "InBlockchain", "http://www.inblockchain.com"]]
+goldsponsors = [["arcc.jpg","ARCC", "http://ibmr.io"], ["zkcapital.png","zkCapital", "https://zk.capital"], ["nucypher.svg","NuCypher", "https://www.nucypher.com"], ["zion.png","City of Zion", "https://cityofzion.io"], ["pillar.png", "Pillar VC", "https://pillar.vc"], ["protocol.png","Protocol Labs", "https://protocol.ai"], ["inblockchain.png", "InBlockchain", "http://www.inblockchain.com"], ["fusion.svg", "Fusion", "https://fusion.org"]]
 goldsponsors = map(lambda x: [sponsorprefix+x[0]]+x[1:],goldsponsors)
 silversponsors = [["jarvis.png","Jarvis", "https://jarvis.ai"], ["shapeshift.png","Shapeshift", "https://www.shapeshift.io"], ["raptor.png","Raptor","http://www.raptorgroup.com"], ["chaincode.png","Chaincode", "http://www.chaincode.com"], ["ledger.png","Ledger Wallet", "https://www.ledgerwallet.com"], ["sia.png", "Sia", "https://siafunds.tech"]]
 silversponsors = map(lambda x: [sponsorprefix+x[0]]+x[1:],silversponsors)
-mediasponsors = [["protocol.png","Protocol Labs", "https://protocol.ai"]]
-mediasponsors = map(lambda x: [sponsorprefix+x[0]]+x[1:],mediasponsors)
 academicsponsors = [["dci.png","Digital Currency Initiative", "http://dci.mit.edu"], ["ben.jpg", "Blockchain Education Network", "https://www.blockchainedu.org"]]
 academicsponsors = map(lambda x: [sponsorprefix+x[0]]+x[1:],academicsponsors)
 with open("index.html", "w") as f:
-    f.write(loader.load("index.tmpl").generate(speakers=speakers,day1_26=day1_26,day1_32=day1_32,day2_26=day2_26,day2_32=day2_32,platinumsponsors=platinumsponsors,goldsponsors=goldsponsors,silversponsors=silversponsors,mediasponsors=mediasponsors,academicsponsors=academicsponsors))
+    f.write(loader.load("index.tmpl").generate(speakers=speakers,day1_26=day1_26,day1_32=day1_32,day2_26=day2_26,day2_32=day2_32,platinumsponsors=platinumsponsors,goldsponsors=goldsponsors,silversponsors=silversponsors,academicsponsors=academicsponsors))
