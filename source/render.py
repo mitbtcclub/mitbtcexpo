51971be9f7924675aea7e44b3123a3a22c4a5c06
Océane Boulais <oceane@mit.edu>
2019-02-27 23:44:52 -0500

    more speakers, changed schedules + sponsors oh my

--- source/render.py
+++ source/render.py
@@ -40,14 +40,16 @@ def generateHTML(prefix):
     sponsorprefix = "./assets/images/sponsors/"
     platinumsponsors = []
     platinumsponsors = map(lambda x: [sponsorprefix+x[0]]+x[1:],platinumsponsors)
-    goldsponsors = [["nash.png","Nash","http://nash.io"]]
+    goldsponsors = [["nash.png","Nash","http://nash.io"],["nebulous.jpg","Nebulous","https://sia.tech/"]]
     goldsponsors = map(lambda x: [sponsorprefix+x[0]]+x[1:],goldsponsors)
-    silversponsors = [["chaincode.png","Chaincode", "http://www.chaincode.com"], ["zcoin.png","ZCoin","https://zcoin.io/"],["FCAT.png","Fidelity Center for Applied Technology","https://www.fidelitylabs.com/"]]
+    silversponsors = [["chaincode.png","Chaincode", "http://www.chaincode.com"], ["zcoin.png","ZCoin","https://zcoin.io/"],["FCAT.png","Fidelity Center for Applied Technology","https://www.fidelitylabs.com/"],["stellar.png","Stellar","https://www.stellar.org/"],["algorand.png","Algorand","https://www.algorand.com/"],["accomplice.png","Accomplice","https://accomplice.co/"]]
     silversponsors = map(lambda x: [sponsorprefix+x[0]]+x[1:],silversponsors)
     academicpartners = [["ben.jpg", "Blockchain Education Network", "https://www.blockchainedu.org"], ["SloanBlockchain.png","http://blockchain.mit.edu/","Sloan Blockchain"],["casa.png","Casa", "https://keys.casa/"], ["dci.png","Digital Currency Initiative", "http://dci.mit.edu"]]
     academicpartners = map(lambda x: [sponsorprefix+x[0]]+x[1:],academicpartners)
+    mediapartners = [["cointelegraph.jpg", "Cointelegraph", "https://cointelegraph.com/"]]
+    mediapartners = map(lambda x: [sponsorprefix+x[0]]+x[1:],mediapartners)
     with open(prefix + "index.html", "wb") as f:
-        f.write(loader.load(prefix + "index.tmpl").generate(speakers=speakers,day1_10=day1_10,day1_34=day1_34,day2_10=day2_10,platinumsponsors=platinumsponsors,goldsponsors=goldsponsors,silversponsors=silversponsors,academicpartners=academicpartners))
+        f.write(loader.load(prefix + "index.tmpl").generate(speakers=speakers,day1_10=day1_10,day1_34=day1_34,day2_10=day2_10,platinumsponsors=platinumsponsors,goldsponsors=goldsponsors,silversponsors=silversponsors,academicpartners=academicpartners,mediapartners=mediapartners))
 
 #generateHTML("flashback/")
 generateHTML("")
